d42d28b285050bcf9850bb1747919a23058decdf
AnibalHigueros <higueros71@gmail.com>
2024-06-10 23:24:40 -0600

    ADD

--- question-app/Models/Questions.cs
+++ question-app/Models/Questions.cs
@@ -1,19 +1,5 @@
 public class Questions {
-    private string _name;
-    private List<Object> _questions;
-
-    public Questions(string name, List<Object> questions) {
-        _name = name;
-        _questions = questions;
-    }
-
-    public List<Object> QuestionsList {
-        get { return _questions; }
-        set { _questions = value; }
-    }
-
-    public string Name {
-        get { return _name; }
-        set { _name = value; }
-    }
+    private string _name { get; set; }
+    private string _description { get; set; }
+    
 }
